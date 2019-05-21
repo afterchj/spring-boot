c5f6c3ee0211314c49dd568ef1802899dfa42b38
hongjian.chen <766256898@qq.com>
2019-05-21 15:58:21 +0800

    补充提交

--- central-console/src/main/java/com/example/blt/CentralConsoleApplication.java
+++ central-console/src/main/java/com/example/blt/CentralConsoleApplication.java
@@ -2,9 +2,16 @@ package com.example.blt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CentralConsoleApplication {
+public class CentralConsoleApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CentralConsoleApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CentralConsoleApplication.class, args);
