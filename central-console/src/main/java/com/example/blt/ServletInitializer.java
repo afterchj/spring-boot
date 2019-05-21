df0bbf7fca72f80de9afbe1284a0d44cb5594c26
hongjian.chen <766256898@qq.com>
2019-05-21 15:28:34 +0800

    。。。

--- central-console/src/main/java/com/example/blt/ServletInitializer.java
+++ central-console/src/main/java/com/example/blt/ServletInitializer.java
@@ -1,6 +1,5 @@
 package com.example.blt;
 
-import com.example.blt.CentralConsoleApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
