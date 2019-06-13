d3cd36e3d94596dfe17538b18b26f58da6ce17da
hongjian.chen <766256898@qq.com>
2019-06-13 18:42:09 +0800

    精简代码

---
+++ ws-blt/src/main/java/com/tpadsz/after/utils/SpringUtils.java
@@ -0,0 +1,33 @@
+package com.tpadsz.after.utils;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by hongjian.chen on 2019/1/22.
+ */
+public class SpringUtils {
+
+    private static ApplicationContext ctx = null;
+
+    static {
+        ctx = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+    }
+
+    public static SqlSessionTemplate getSqlSession() {
+        return (SqlSessionTemplate) ctx.getBean("mySqlSessionTemplate");
+    }
+
+//    public static AmqpTemplate getAmqpTemplate() {
+//        return (AmqpTemplate) ctx.getBean("amqpTemplate");
+//    }
+
+//    public static MessageProducer getProducer() {
+//        return ctx.getBean(MessageProducer.class);
+//    }
+
+//    public static void main(String[] args) {
+//        System.out.println("mySqlSessionTemplate="+getSqlSession());
+//    }
+}
