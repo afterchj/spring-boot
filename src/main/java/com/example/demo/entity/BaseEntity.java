9735ef6b74d8e6ea474a19f769cb21315cda2049
after <afterchj@foxmail.com>
2021-01-02 12:13:32 +0800

    优化表结构设计

---
+++ src/main/java/com/example/demo/entity/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.example.demo.entity;
+
+import lombok.Data;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * @Author: hjchen
+ * @Contact: afterchj@foxmail.com
+ * @Date: 2020/12/21 13:49
+ */
+@Data
+@MappedSuperclass
+public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+
+    private LocalDateTime createTime = LocalDateTime.now();
+
+    private LocalDateTime updateTime = LocalDateTime.now();
+}
--- src/main/java/com/example/demo/entity/Info.java
+++ src/main/java/com/example/demo/entity/Info.java
@@ -7,25 +7,14 @@ import javax.persistence.*;
  */
 
 @Entity
-public class Info {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class Info extends BaseEntity{
+
 
     @Column(nullable = false,unique =true)
     private String userName;
 
     @Column
     private String password;
-
-    public Long getId() {
-        return id;
-    }
-
-
-    public void setId(Long id) {
-        this.id = id;
-    }
 
     public String getUserName() {
         return userName;
